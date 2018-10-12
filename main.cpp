b55eb0d144671d1c72f20eef3511318297d41e3f
Eragoneq <eragon.skill@gmail.com>
2018-10-12 12:15:09 +0200

    Szyfrek cezarka

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int klucz;
+    std::string slowo, szyfr;
+
+    std::cout << "Podaj slowo do zaszyfrowania i liczbe jako klucz: \n";
+    std::cin >> slowo;
+    std::cin >> klucz;
+
+    klucz=klucz%26;
+
+    for (char i : slowo) {
+        szyfr.append(1, char(int(i) + klucz));
+    }
+    std::cout << szyfr;
 }
