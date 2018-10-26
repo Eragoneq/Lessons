b0fc4d3d9a7c971c1d74a48dd046513216e8ff82
Eragoneq <eragon.skill@gmail.com>
2018-10-26 23:53:06 +0200

    Naprawienie bledu z wyznacznikami

--- rozwiaz_uklad.cpp
+++ rozwiaz_uklad.cpp
@@ -2,15 +2,21 @@
 #include <array>
 
 float wyznacznik(float a, float b, float c, float d){
-    return (a*c)-(b*d);
+    return (a*d)-(b*c);
 }
 
-std::array<float, 2> rozwiazRownanie(float a, float b, float c, float d, float e, float f){
+std::array<float, 2> rozwiazRownanie(float a, float b, float c,
+                                     float d, float e, float f){
     std::array<float, 2> wynik{};
+
     float W, Wx, Wy;
-    W = wyznacznik(a,b,c,d);
-    Wx = wyznacznik(e,b,f,d);
-    Wy = wyznacznik(a,e,c,f);
+
+    W  = wyznacznik(a,b,
+                    d,e);
+    Wx = wyznacznik(c,b,
+                    f,e);
+    Wy = wyznacznik(a,c,
+                    d,f);
 
     if(W!=0){
         wynik[0]=Wx/W;
@@ -26,7 +32,7 @@ std::array<float, 2> rozwiazRownanie(float a, float b, float c, float d, float e
 void runRozwiazRownanie(){
     float a,b,c,d,e,f;
     std::array<float, 2> wynik{};
-    std::cout << "Podaj dla: ax+by=e, cx+dy=f\nW kolejnosci a, b, c, d, e, f\n";
+    std::cout << "Podaj dla: ax+by=c, dx+ey=f\nW kolejnosci a, b, c, d, e, f\n";
     std::cin >> a >> b >> c >> d >> e >> f;
     wynik = rozwiazRownanie(a,b,c,d,e,f);
     std::cout << "X = " << wynik[0] << "\n";
