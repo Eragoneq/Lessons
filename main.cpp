e74e21fca2800e7a1fd1bb36d31e7032a78dfff4
Eragoneq <eragon.skill@gmail.com>
2018-11-05 10:37:51 +0100

    Pierwiastki

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 #include "suma_cyfr.cpp"
 #include "rozwiaz_uklad.cpp"
 #include "obliczanie_wielomianu.cpp"
+#include "pierwiastek.cpp"
 
 int main() {
-    runObliczWielomian();
+    runPierwiastek();
 }
